3068b856f7bda01ee7f0be09821d1e95089626f2
Tarssito <tarssito@gmail.com>
2018-09-16 11:39:09 -0300

    Configurando Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
@@ -13,9 +13,10 @@ import { environment } from '../environments/environment.prod';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
