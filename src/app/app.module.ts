8787159cda0e528bc40cd4fa2051ce0b6c453fdf
Tarssito <tarssito@gmail.com>
2018-09-16 12:42:46 -0300

    Container component para lista de tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { MatToolbarModule } from '@angular/material';
+import {
+  MatLineModule,
+  MatListModule,
+  MatSlideToggleModule,
+  MatToolbarModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    MatLineModule,
+    MatListModule,
+    MatSlideToggleModule,
     MatToolbarModule
   ],
   providers: [],
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
+})
+export class TaskListComponent implements OnInit {
+
+  tasks: Task[] = [
+    {
+      uid: '',
+      title: 'Learn Angular',
+      done: false
+    },
+    {
+      uid: '',
+      title: 'Learn Firebase',
+      done: false
+    }
+  ];
+  selectedTask: Task;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onPerformTask(task: Task): void {
+    console.log(task);
+  }
+}
