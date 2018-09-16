5fbbf7a74627c86ac5a6f58d174f44cb81aa0191
Tarssito <tarssito@gmail.com>
2018-09-16 13:30:54 -0300

    Criando metodos crud em TaskService firebase

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 import { Task } from './models/task.model';
+
 
 @Injectable()
 export class TaskService {
@@ -16,5 +18,27 @@ export class TaskService {
 
   private setTaks(): void {
     this.tasks = this.db.collection<Task>('/tasks');
+  }
+
+  create(task: Task): Promise<void> {
+    const uid = this.db.createId();
+    return this.tasks.doc<Task>(uid)
+      .set({
+        uid,
+        title: task.title,
+        done: false
+      });
+  }
+
+  update(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid).update(task);
+  }
+
+  delete(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid).delete();
+  }
+
+  get(uid: string): Observable<Task> {
+    return this.tasks.doc<Task>(uid).valueChanges();
   }
 }
